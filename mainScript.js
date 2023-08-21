e1231cc610ecbc439212d1a0add680777bb18855
whitecoffin <68064525+whitecoffin@users.noreply.github.com>
2023-08-21 16:11:14 +0900

    縦方向の結合を追加
    従来の関数を分け、縦と横の2種類の結合に対応

--- mainScript.js
+++ mainScript.js
@@ -106,7 +106,57 @@ async event => {
 
 
 // 画像を取得して新しい画像を生成する
-function GenerateNewImg() {
+
+// 下側に結合
+function GenerateNewImgVertical() {
+  // 生成されたimgタグに共通するクラス名で要素を全取得
+  var getImagesElement = document.getElementsByClassName("previewImg");
+  // 処理内でのみ使うcanvasの作成
+  var newImgcv = document.createElement("canvas");
+  var ct = newImgcv.getContext("2d");
+  var cvWidth = 0;
+  var cvHeight = 0;
+  // 生成後canvasの横と縦の大きさを求める
+  for (i = 0; i < getImagesElement.length; i++) {
+    // 縦幅は要素の和
+    cvHeight += getImagesElement[i].naturalHeight;
+    if (cvWidth < getImagesElement[i].naturalWidth) {
+      // 横幅は要素の最大値
+      cvWidth = getImagesElement[i].naturalWidth;
+    }
+  }
+  // canvasのサイズを設定
+  newImgcv.width = cvWidth;
+  newImgcv.height = cvHeight;
+  ct.fillStyle = "rgba(0,0,0,0)";
+  ct.fillRect(0,0,newImgcv.width,newImgcv.height);
+
+  // 要素数分描画を行う
+  for (i = 0; i < getImagesElement.length; i++) {
+    //描画を行う位置
+    var drawPointX = 0;
+    var drawPointY = 0;
+    if (i == 0) {
+      // 1枚目は何も行わない
+    } else {
+      // 2枚目以降はそれまでの画像の幅だけ下にずらして描画する
+      for (j = 0; j < i; j++) {
+        drawPointY += getImagesElement[j].naturalHeight;
+      }
+    }
+    // 描画
+    ct.drawImage(getImagesElement[i],drawPointX,drawPointY);
+  }
+  // canvasの中身をimgに渡す
+  var resultCv = newImgcv.toDataURL("image/png");
+  document.getElementById("resultImg").src = resultCv;
+  // 結果表示divの表示切替
+  var resultDiv = document.getElementById("resultDiv");
+  changeVisible(resultDiv);
+}
+
+// 右側に結合
+function GenerateNewImgHorizontal() {
   // 生成されたimgタグに共通するクラス名で要素を全取得
   var getImagesElement = document.getElementsByClassName("previewImg");
   // 処理内でのみ使うcanvasの作成
